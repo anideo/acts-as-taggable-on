5d61df52623fe0950cdcf99a831508bdd2dacb76
Raphael Sofaer <raphael@joindiaspora.com>
2011-03-11 13:28:53 -0800

    Stop using LIKE to do case insensitivity, instead just downcase everything on the way in.  Tests now pass with a utf8_bin collation in MYSQL

--- lib/acts_as_taggable_on/tag.rb
+++ lib/acts_as_taggable_on/tag.rb
@@ -1,7 +1,7 @@
 module ActsAsTaggableOn
   class Tag < ::ActiveRecord::Base
     include ActsAsTaggableOn::ActiveRecord::Backports if ::ActiveRecord::VERSION::MAJOR < 3
-  
+
     attr_accessible :name
 
     ### ASSOCIATIONS:
@@ -14,30 +14,31 @@ module ActsAsTaggableOn
     validates_uniqueness_of :name
 
     ### SCOPES:
-    
+
     def self.using_postgresql?
       connection.adapter_name == 'PostgreSQL'
     end
 
     def self.named(name)
-      where(["name #{like_operator} ?", name])
+      where(["name = ?", comparable_name(name)])
     end
-  
+
     def self.named_any(list)
-      where(list.map { |tag| sanitize_sql(["name #{like_operator} ?", tag.to_s]) }.join(" OR "))
+      where(list.map { |tag| sanitize_sql(["name = ?", comparable_name(tag.to_s)]) }.join(" OR "))
     end
-  
+
     def self.named_like(name)
-      where(["name #{like_operator} ?", "%#{name}%"])
+      where(["name #{like_operator} ?", "%#{comparable_name(name)}%"])
     end
 
     def self.named_like_any(list)
-      where(list.map { |tag| sanitize_sql(["name #{like_operator} ?", "%#{tag.to_s}%"]) }.join(" OR "))
+      where(list.map { |tag| sanitize_sql(["name #{like_operator} ?", "%#{comparable_name(tag.to_s)}%"]) }.join(" OR "))
     end
 
     ### CLASS METHODS:
 
     def self.find_or_create_with_like_by_name(name)
+      name = comparable_name(name)
       named_like(name).first || create(:name => name)
     end
 
@@ -47,7 +48,7 @@ module ActsAsTaggableOn
       return [] if list.empty?
 
       existing_tags = Tag.named_any(list).all
-      new_tag_names = list.reject do |name| 
+      new_tag_names = list.reject do |name|
                         name = comparable_name(name)
                         existing_tags.any? { |tag| comparable_name(tag.name) == name }
                       end
@@ -57,6 +58,10 @@ module ActsAsTaggableOn
     end
 
     ### INSTANCE METHODS:
+
+    def name= new_name
+      write_attribute(:name, self.class.comparable_name(new_name))
+    end
 
     def ==(object)
       super || (object.is_a?(Tag) && name == object.name)
@@ -71,13 +76,13 @@ module ActsAsTaggableOn
     end
 
     class << self
+      def comparable_name(str)
+        RUBY_VERSION >= "1.9" ? str.downcase : str.mb_chars.downcase
+      end
+
       private
         def like_operator
           using_postgresql? ? 'ILIKE' : 'LIKE'
-        end
-        
-        def comparable_name(str)
-          RUBY_VERSION >= "1.9" ? str.downcase : str.mb_chars.downcase
         end
     end
   end
